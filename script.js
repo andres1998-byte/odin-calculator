8cebaf1b96c5f37fbe10669fd668dc544667d0ad
Andres Villarreal <amvillarreal98@gmail.com>
2023-05-08 17:59:33 -0600

    Create html boilerplate and write operating functions

---
+++ script.js
@@ -0,0 +1,36 @@
+function add (a,b) {
+    return a+b;
+}
+
+function subtract (a,b) {
+    return a-b;
+}
+
+function multiply (a,b) {
+    return a*b;
+}
+
+function divide (a,b) {
+    return a/b;
+}
+
+function operate (a,b,operator) {
+    if (operator=="add") {
+        return add(a,b);
+    }
+
+    if (operator=="subtract") {
+        return subtract(a,b);   
+    }
+
+    if (operator=="multiply") {
+        return multiply(a,b);       
+    }
+
+    if (operator=="divide") {
+        return divide(a,b);
+        
+    }
+}
+
+
